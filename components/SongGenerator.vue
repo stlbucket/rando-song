4f5bd54dcbe30e5021e90cf006cad3a8c6fdb7ff
Kevin Burkett <stlbucket@gmail.com>
2025-02-07 15:17:52 -0800

    sync

--- components/SongGenerator.vue
+++ components/SongGenerator.vue
@@ -6,11 +6,12 @@
         {
           // label: 'Attribute',
           key: 'name',
-          class: 'w-[400px]'
+          class: 'w-[100px]'
         },
         {
           // label: 'Value',
           key: 'value',
+          class: 'text-wrap w-[200px]'
         }
       ]"
       :rows="songParameters"
@@ -35,6 +36,18 @@
       return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
+      const keyCenter = keyCenters[randomIntFromInterval(0, 19)]
+      const basicTonality = basicTonalities[randomIntFromInterval(0, 19)]
+      let expandedTonality =  expandedToanalities[randomIntFromInterval(0, 19)]
+      if (expandedTonality === 'dorian if minor, mixolydian if major') {
+        if (basicTonality === 'minor') expandedTonality = 'dorian'
+        else if (basicTonality === 'major') expandedTonality = 'mixolydian'
+        else expandedTonality = 'mixed basic tonality'
+      }
+
+      const chordMovement = chordMovements[randomIntFromInterval(0, 19)]
+      const tempo = tempos[randomIntFromInterval(0, 19)]
+
     let meter = meters[randomIntFromInterval(0, 19)]
     if (meter === 'odd') {
       const oddMeterTop = oddMeterTops[randomIntFromInterval(0,19)]
@@ -43,11 +56,11 @@
     }
 
     songParameters.value = [
-      {name: 'Key Center', value: keyCenters[randomIntFromInterval(0, 19)]},
-      {name: 'Basic Tonality', value: basicTonalities[randomIntFromInterval(0, 19)]},
-      {name: 'Expanded Tonality', value: expandedToanalities[randomIntFromInterval(0, 19)]},
-      {name: 'Chord Movement', value: chordMovements[randomIntFromInterval(0, 19)]},
-      {name: 'Tempo', value: tempos[randomIntFromInterval(0, 19)]},
+      {name: 'Key Center', value: keyCenter},
+      {name: 'Basic Tonality', value: basicTonality},
+      {name: 'Expanded Tonality', value: expandedTonality},
+      {name: 'Chord Movement', value: chordMovement},
+      {name: 'Tempo', value: tempo},
       {name: 'Meter', value: meter},
   ]
   }
